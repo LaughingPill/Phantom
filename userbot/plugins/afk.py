8b63fc7e1d10321526e4f1ab27998e5251960985
LaughingPill <59218577+LaughingPill@users.noreply.github.com>
2020-05-20 14:58:09 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -121,7 +121,7 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"My Master Has Been Gone For {afk_since}\nWhere He Is: Probably Studying or Watching Netflix " + \
+        message_to_reply = f"Yo,My Master Has Been Gone For {afk_since}\nWhere He Is: Its Lockdown man he's in his house ♪～(´ε｀ ) " + \
             f"\n\n__ He'll back in a few hours!__\n**REASON**: {reason}" \
             if reason \
             else f"**Important Notice**\n\n[This User Is Ded Forever...](https://telegra.ph//file/a53fa950ff31781d5930a.jpg) "
